837633934b8e9050ad90e3657c20cada13e39f8e
ktsstd <rlaxotjd0403@gmail.com>
2024-06-09 01:25:15 +0900

    06.08 작업
    적 코드 수정,
    스테이지 맵 추가(블렌더 안깔려있으면 안보임)

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -4,11 +4,59 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    bool isPlayerFind = false;
 
-    private void Start()
+    private void FixedUpdate()
     {
+        ViewRange();
+    }
+
+    public Vector3 boxSize = new Vector3(5, 2, 5);
+
+    void ViewRange()
+    {
+        Vector3 boxCentar = transform.position + transform.TransformDirection(Vector3.forward * 4);
+        Collider[] viewBox = Physics.OverlapBox(boxCentar, boxSize, transform.rotation);
+
+        bool isPlayerIn = false;
+
+        foreach (Collider collider in viewBox)
+        {
+            if (collider.tag == "Player")
+            {
+                isPlayerIn = true;
+                RaycastHit hit;
+                Physics.Raycast(transform.position, collider.transform.position - transform.position, out hit);
+                Debug.DrawRay(transform.position, collider.transform.position - transform.position);
+
+                if (hit.collider.tag == "Player")
+                {
+                    isPlayerFind = true;
+                    transform.GetChild(1).gameObject.SetActive(true);
+                }
+            }
+        }
+
+        if (isPlayerIn && isPlayerFind == false)
+            transform.GetChild(0).gameObject.SetActive(true);
+        else
+            transform.GetChild(0).gameObject.SetActive(false);
 
     }
 
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(Vector3.zero + (Vector3.forward * 4), boxSize * 2);
+    }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("전투 진입");
+            // 전투 진입
+        }
+    }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -14,22 +14,28 @@ public class PlayerMovement : MonoBehaviour
 
     private bool isMove = false;
 
-    void Update() {
-        if (Input.GetMouseButtonDown(0)) {
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
             CheckRaycast();
         }
-        if (isMove) {
+        if (isMove)
+        {
             transform.position += dir.normalized * Time.deltaTime * speed;
             transform.DORotate(lookTarget.eulerAngles, 1);
             isMove = (transform.position - destPos).magnitude > 0.05f;
         }
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
 
-    public void CheckRaycast() {
+    public void CheckRaycast()
+    {
         RaycastHit hit;
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit, 100f)) {
+        if (Physics.Raycast(ray, out hit, 100f))
+        {
             destPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
             dir = destPos - transform.position;
             lookTarget = Quaternion.LookRotation(dir);
